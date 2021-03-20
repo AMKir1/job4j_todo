d9bd5824b749c05f1f571a3bc9f75af7c7383c1e
Andrei Kirillovykh <andykirill@gmail.com>
2021-03-20 13:46:21 +0300

    3. Лямбды и шаблон wrapper.[49295#453803]

--- src/main/java/ru/job4j/hibernate/Hibernate.java
+++ src/main/java/ru/job4j/hibernate/Hibernate.java
@@ -2,6 +2,7 @@ package ru.job4j.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -10,6 +11,8 @@ import ru.job4j.store.Store;
 
 
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Hibernate implements Store, AutoCloseable {
 
@@ -25,61 +28,64 @@ public class Hibernate implements Store, AutoCloseable {
 
     @Override
     public Item add(Item item) {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        s.save(item);
-        s.getTransaction().commit();
-        s.close();
+        this.upsert(s -> s.save(item));
         return item;
     }
 
     @Override
     public Item findById(long id) {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        Item item = s.get(Item.class, id);
-        s.getTransaction().commit();
-        s.close();
-        return item;
+        return this.get(s -> s.get(Item.class, id));
     }
 
     @Override
     public Item update(Item item) {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        s.update(item);
-        s.getTransaction().commit();
-        s.close();
+        this.upsert(s -> s.update(item));
         return item;
     }
 
     @Override
     public void delete(Item item) {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        s.delete(item);
-        s.getTransaction().commit();
-        s.close();
+        this.upsert(s -> s.delete(item));
     }
 
     @Override
     public List<Item> allItems() {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        List items = s.createQuery("from ru.job4j.model.Item order by created desc").list();
-        s.getTransaction().commit();
-        s.close();
-        return items;
+        return this.get(s -> s.createQuery("from ru.job4j.model.Item").list());
     }
 
     @Override
     public List<Item> allNotDoneItems() {
-        Session s = sf.openSession();
-        s.beginTransaction();
-        List doneitems = s.createQuery("from ru.job4j.model.Item where done = false order by created desc").list();
-        s.getTransaction().commit();
-        s.close();
-        return doneitems;
+        return this.get(s -> s.createQuery("from ru.job4j.model.Item where done = false order by created desc").list());
+    }
+
+    private <T> T get(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
+    private void upsert(final Consumer<Session> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            command.accept(session);
+            tx.commit();
+
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
 }
