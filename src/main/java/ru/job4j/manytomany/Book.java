1894b4799bf428203531563d311f77c2bf9b2186
Andrei Kirillovykh <andykirill@gmail.com>
2021-03-22 16:42:07 +0300

    2. ManyToMany[331986#453810]

---
+++ src/main/java/ru/job4j/manytomany/Book.java
@@ -0,0 +1,65 @@
+package ru.job4j.manytomany;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Book of(String name) {
+        Book book = new Book();
+        book.setName(name);
+        return book;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id &&
+                Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
